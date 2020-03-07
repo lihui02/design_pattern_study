f3ee9542dba259495f4b5f20fbddd10b94c0417f
imooc <876870723@qq.com>
2020-03-07 17:33:22 +0800

    增加avl二叉树的旋转和遍历打印代码

--- src/main/java/com/lihui/study/algorithms/AVLTree.java
+++ src/main/java/com/lihui/study/algorithms/AVLTree.java
@@ -7,7 +7,8 @@ package com.lihui.study.algorithms;
  * @date: 2020-3-6  11:11
  */
 
-public class AVLTree<E extends Comparable<? super E>> {
+public class
+AVLTree<E extends Comparable<? super E>> {
     private AVLNode<E> root;
 
     private static class AVLNode<E> {
@@ -60,12 +61,14 @@ public class AVLTree<E extends Comparable<? super E>> {
             return new AVLNode<>(e, null, null);
         }
         int result = e.compareTo(avlNode.element);
+        if (result==0) return avlNode;
         if (result < 0) {
             avlNode.left = insert(e, avlNode.left);
 
         } else if (result > 0) {
             avlNode.right = insert(e, avlNode.right);
         }
+        avlNode.height = Math.max(getHeight(avlNode.left), getHeight(avlNode.right)) + 1;
         return balance(avlNode);
     }
 
@@ -87,53 +90,97 @@ public class AVLTree<E extends Comparable<? super E>> {
             }
             //第二种边界情况，左儿子的右树长了
             if (getHeight(avlNode.left.left) < getHeight(avlNode.left.right)) {
-
+                avlNode = doubleRotateWithLeftRightChild(avlNode);
             }
         } else if (getHeight(avlNode.right) - getHeight(avlNode.left) > ALLOWED_IMBALANCE) {
 
             if (getHeight(avlNode.left) - getHeight(avlNode.right) > ALLOWED_IMBALANCE) {
                 //第三种边界情况，右儿子的左树长了
                 if (getHeight(avlNode.right.left) > getHeight(avlNode.right.right)) {
-
+                    avlNode = doubleRotateWithRightLeftChild(avlNode);
                 }
                 //第四种边界情况，右儿子的右树长了
                 if (getHeight(avlNode.right.left) < getHeight(avlNode.right.right)) {
                     avlNode = rotateWithRightChild(avlNode);
                 }
-
             }
+        }else {
+            //不需要平衡
         }
         return avlNode;
     }
 
     /**
-     * 左树单旋转
+     * 左-左树单旋转
+     *
      * @param k2
      * @return
      */
     private AVLNode<E> rotateWithLeftChild(AVLNode<E> k2) {
         AVLNode<E> k1 = k2.left;
-        k2.left=k1.right;
-        k1.right=k2;
-        k2.height=Math.max(getHeight(k2.left),getHeight(k2.right))+1;
-        k1.height=Math.max(getHeight(k1.left),getHeight(k1.right))+1;
+        k2.left = k1.right;
+        k1.right = k2;
+        k2.height = Math.max(getHeight(k2.left), getHeight(k2.right)) + 1;
+        k1.height = Math.max(getHeight(k1.left), getHeight(k1.right)) + 1;
         return k1;
     }
+
     /**
-     * 右树单旋转
+     * 左-右树双旋转
+     *
+     * @param k3
+     * @return
+     */
+    private AVLNode<E> doubleRotateWithLeftRightChild(AVLNode<E> k3) {
+      k3.left=rotateWithRightChild(k3.left);
+      return rotateWithLeftChild(k3);
+    }
+
+    /**
+     * 右-右树单旋转
+     *
      * @param k2
      * @return
      */
     private AVLNode<E> rotateWithRightChild(AVLNode<E> k2) {
         AVLNode<E> k1 = k2.right;
-        k2.right=k1.left;
-        k1.left=k2;
-        k2.height=Math.max(getHeight(k2.left),getHeight(k2.right))+1;
-        k1.height=Math.max(getHeight(k1.left),getHeight(k1.right))+1;
+        k2.right = k1.left;
+        k1.left = k2;
+        k2.height = Math.max(getHeight(k2.left), getHeight(k2.right)) + 1;
+        k1.height = Math.max(getHeight(k1.left), getHeight(k1.right)) + 1;
         return k1;
     }
 
+    /**
+     * 右-左树双旋转
+     *
+     * @param k1
+     * @return
+     */
+    private AVLNode<E> doubleRotateWithRightLeftChild(AVLNode<E> k1) {
+        k1.right=rotateWithLeftChild(k1.right);
+        return rotateWithRightChild(k1);
+    }
+
+    private void print(AVLNode node,String a){
+        a=a+a;
+        if (node==null) return;
+        print(node.left,a);
+        System.out.println(a+""+node.element);
+        print(node.right,a);
+    }
+    public void print(){
+        print(this.root,"-");
+    }
     public static void main(String[] args) {
+        AVLTree<Integer> tree = new AVLTree<>();
+         tree.insert(7);
+         tree.insert(4);
+         tree.insert(10);
+         tree.insert(3);
+         tree.insert(5);
+         tree.insert(6);
+         tree.print();
 
 
     }
